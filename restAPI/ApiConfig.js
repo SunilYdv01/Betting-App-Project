2acc349b6e0b55e1aad8da60c74ad55158431506
SunilYdv01 <95336927+SunilYdv01@users.noreply.github.com>
2025-03-22 13:03:55 +0530

    Add files via upload

---
+++ restAPI/ApiConfig.js
@@ -0,0 +1,29 @@
+// let baseurl;
+let url = ' http://182.72.203.247:4094';
+
+const ApiConfig = {
+  login: `${url}/auth`,
+  //   signup: `${url}/api/v1/user/signUp`,
+  //   login: `${url}/api/v1/user/loginUser`,
+  //   verifyOtp: `${url}/api/v1/user/verifyOtp`,
+  //   resendTop: `${url}/api/v1/user/resendOtp`,
+  // static data
+  //   StaticData: `${url}/api/v1/user/getStatic`,
+
+  // home slider data
+  //   getHomeBannerData: `${url}/api/v1/user/getBanner`,
+
+  // getcategory home
+  //   getCategoryHome: `${url}/api/v1/user/getcategoryList`,
+
+  //get service data from service home screen
+  //   getServiceData: `${url}/api/v1/user/getServiceList`,
+
+  // Privacy Policy API
+  //   privacypolicy: `${url}/api/v1/user/getStatic`,
+
+  //get profile data
+  //   getProfileData: `${url}/api/v1/user/myProfile`,
+};
+
+export default ApiConfig;
